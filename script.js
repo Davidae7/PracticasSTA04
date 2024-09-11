483e39dcde67416082c497e8b69dd7f297652c3a
System User <sta04@TLM141.net.tlm.unavarra.es>
2024-09-11 17:43:42 +0200

    he modificado todo

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert('¡Hola!');
+document.getElementById("buscar").addEventListener('click', function(){
+
+    fetch('http://www.omdbapi.com/?apikey=8053f2df&t=pokemon')
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+})
